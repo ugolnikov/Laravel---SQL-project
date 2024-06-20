61c8bcf633b45aee58be316ed992ff3b021afb33
ugolnikov <42218732+ugolnikov@users.noreply.github.com>
2024-06-20 09:01:50 +0500

    Update students.blade.php

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -1,16 +1,28 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="flex w-full text-center justify-evenly">
+        <a href="#" class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('List of all students') }}
+        </a>
+        <a href="#" class="font-thin text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Create new one') }}
+        </a></div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="text-3xl py-3">{{ __("Students!") }}</h1>
-                    <table class="table-fixed w-full text-center">
+                <div class="p-6 "  data-bs-theme="dark">
+                    <h1 class="text-3xl py-3 text-gray-900 dark:text-gray-100">{{ __("Students!") }}</h1>
+                    <select class="form-select rounded mb-5">
+                        <option value="" selected disabled hidden>Choose here</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                        <option value="4">Four</option>
+                        <option value="5">Five</option>
+                    </select>
+                    <table class="table-fixed w-full text-center text-gray-900 dark:text-gray-100">
                         <thead>
                             <tr>
                                 <th class="w-1/6">{{ __("Id") }}</th>
